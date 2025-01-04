ba9f495494740cfea7f399e74be3b07ae0b5f1f2
AjaySRawat07 <ajju246455@gmail.com>
2025-01-04 22:46:30 +0530

    landing

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           {/* footer */}
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600 font-medium">
-              <p>Made by Harsh & Ajju🚀🚀</p>
+              <p>Made by Harsh & Ajay🚀🚀</p>
             </div>
           </footer>
         </body>
